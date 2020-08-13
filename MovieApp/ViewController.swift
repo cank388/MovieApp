053a59c7a47a411bd366b3efe3802fcc86096b43
Can <can@basefy.com>
2020-08-13 17:57:55 +0300

    Initial Commit

---
+++ MovieApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by Can Kalender on 13.08.2020.
+//  Copyright © 2020 Can Kalender. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
