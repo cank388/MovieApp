77d4a93784f9638de0abf8e00b443623e7a070b7
Can <can@basefy.com>
2020-08-16 21:10:27 +0300

    Pods added

---
+++ MovieApp/Scenes/MovieMainPage/MovieViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MovieViewController.swift
+//  MovieApp
+//
+//  Created by Can Kalender on 16.08.2020.
+//  Copyright © 2020 Can Kalender. All rights reserved.
+//
+
+import UIKit
+
+class MovieViewController: UIViewController{
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Anasayfa"
+        
+    }
+}
